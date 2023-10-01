208695b0389fa8b90f33725b44e482661abf3618
leonelborgesdev <leonel03t@gmail.com>
2023-10-01 18:37:51 -0400

    tienda

--- src/components/Tienda/Tienda.jsx
+++ src/components/Tienda/Tienda.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Nav } from "../Nav/Nav";
 import { useSelector } from "react-redux";
+import { Card } from "../Card/Card";
 
 export const Tienda = () => {
   const products = useSelector((state) => state.products);
@@ -8,6 +9,13 @@ export const Tienda = () => {
     <div>
       <Nav />
       Tienda
+      {products.list.length > 0 ? (
+        products.list.map((product) => {
+          return <Card product={product} key={product.id} />;
+        })
+      ) : (
+        <span>No se encontro productos</span>
+      )}
     </div>
   );
 };
--- src/store/slices/products/index.js
+++ src/store/slices/products/index.js
@@ -5,31 +5,45 @@ export const productsSlice = createSlice({
   initialState: {
     list: [
       {
+        id: 1,
         marca: "Rayban",
         precio: 350,
         descripcion: "rojo-azul de acetato",
         stock: 1,
       },
       {
+        id: 2,
         marca: "Rayban",
         precio: 350,
         descripcion: "negro-verde de acetato",
         stock: 2,
       },
       {
+        id: 3,
         marca: "ECO",
         precio: 350,
         descripcion: "negro-azul de acetato",
         stock: 2,
       },
       {
+        id: 4,
         marca: "ECO",
         precio: 350,
         descripcion: "negro-gris de acetato",
         stock: 2,
       },
-      { precio: 10, descripcion: "liquido para lentes", stock: 20 },
-      { precio: 80, descirpcion: "liquido para lentes de contacto", stock: 13 },
+      {
+        id: 5,
+        precio: 10,
+        descripcion: "liquido para lentes",
+        stock: 20,
+      },
+      {
+        id: 6,
+        precio: 80,
+        descripcion: "liquido para lentes de contacto",
+        stock: 13,
+      },
     ],
   },
   reducers: {},
