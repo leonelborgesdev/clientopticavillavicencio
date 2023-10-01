8f2b123c4493cbff8605e1bb4527e1676c09b8f2
leonelborgesdev <leonel03t@gmail.com>
2023-10-01 19:13:25 -0400

    css tienda

--- src/components/Tienda/Tienda.jsx
+++ src/components/Tienda/Tienda.jsx
@@ -2,20 +2,25 @@ import React from "react";
 import { Nav } from "../Nav/Nav";
 import { useSelector } from "react-redux";
 import { Card } from "../Card/Card";
+import "./Tienda.css";
 
 export const Tienda = () => {
   const products = useSelector((state) => state.products);
   return (
     <div>
       <Nav />
-      Tienda
-      {products.list.length > 0 ? (
-        products.list.map((product) => {
-          return <Card product={product} key={product.id} />;
-        })
-      ) : (
-        <span>No se encontro productos</span>
-      )}
+      <div className="container_products">
+        <h1>Tienda</h1>
+        <div className="contanier_cards">
+          {products.list.length > 0 ? (
+            products.list.map((product) => {
+              return <Card product={product} key={product.id} />;
+            })
+          ) : (
+            <span>No se encontro productos</span>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
