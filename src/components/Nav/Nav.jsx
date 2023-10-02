93c5264e58c302e386924903f69424108ff4fa0d
leonelborgesdev <leonel03t@gmail.com>
2023-10-02 11:26:07 -0400

    estilos nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Nav.css";
 
 export const Nav = () => {
   return (
-    <div>
+    <div className="container_nav">
       <Link to={"/"}>Home</Link>
       <Link to={"/tienda"}>Tienda</Link>
       <Link to={"/cita"}>Reservar Cita</Link>
