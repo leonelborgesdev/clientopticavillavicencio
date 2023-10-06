9856e8894f1dc5a73b545661e0239bb1fb20e4b0
leonelborgesdev <leonel03t@gmail.com>
2023-10-06 08:39:28 -0400

    citas

--- src/components/Citas/Citas.jsx
+++ src/components/Citas/Citas.jsx
@@ -5,7 +5,23 @@ export const Citas = () => {
   return (
     <div>
       <Nav />
-      Citas
+      <h1>Reservar Cita</h1>
+      <div>
+        <h2>Cliente</h2>
+        <div>
+          <h4>Nombre:</h4>
+          <input type="text" />
+          <h4>Cel/Tel:</h4>
+          <input type="text" />
+        </div>
+      </div>
+      <div>
+        <h2>Cita</h2>
+        <h4>Fecha:</h4>
+        <input type="text" />
+        <h4>Hora:</h4>
+        <input type="text" />
+      </div>
     </div>
   );
 };
---
+++ src/components/Cliente/Cliente.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Cliente = () => {
+  return <div>Cliente</div>;
+};
