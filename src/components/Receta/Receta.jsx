74561c301be9ef668c8b2d9f8dec351ae8e30221
leonelborgesdev <leonel03t@gmail.com>
2023-10-12 11:00:42 -0400

    recipe

--- src/components/Receta/Receta.jsx
+++ src/components/Receta/Receta.jsx
@@ -1,21 +1,64 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 
 export const Receta = () => {
+  const client = useSelector((state) => state.client);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleSave = () => {
+    navigate("/tienda");
+  };
   return (
     <div>
       <Link to="/tienda">Volver</Link>
-      <h1>Receta</h1>
-      <h3>Doctor/a:</h3>
-      <input type="text" />
-      <h3>Od</h3>
-      <input type="text" />
-      <input type="text" />
-      <input type="text" />
-      <h3>Oi</h3>
-      <input type="text" />
-      <input type="text" />
-      <input type="text" />
+      <div>
+        <h1>Cliente</h1>
+        <h3>Nombre:</h3>
+        <h3>{client.obj.nombre}</h3>
+        <h3>Tel/Cel.:</h3>
+        <h3>{client.obj.nombre}</h3>
+      </div>
+      <div>
+        <h1>Receta</h1>
+        <h3>Doctor/a:</h3>
+        <input type="text" />
+        <h3>Lejos</h3>
+        <h3>Od</h3>
+        <span>Esf.</span>
+        <input type="text" />
+        <span>Cil.</span>
+        <input type="text" />
+        <span>Eje.</span>
+        <input type="text" />
+        <h3>Oi</h3>
+        <span>Esf.</span>
+        <input type="text" />
+        <span>Cil.</span>
+        <input type="text" />
+        <span>Eje.</span>
+        <input type="text" />
+
+        <h3>Cerca</h3>
+
+        <span>ADD.</span>
+        <input type="text" />
+        <h3>Od</h3>
+        <span>Esf.</span>
+        <input type="text" />
+        <span>Cil.</span>
+        <input type="text" />
+        <span>Eje.</span>
+        <input type="text" />
+        <h3>Oi</h3>
+        <span>Esf.</span>
+        <input type="text" />
+        <span>Cil.</span>
+        <input type="text" />
+        <span>Eje.</span>
+        <input type="text" />
+        <button onClick={() => handleSave()}>Guardar</button>
+      </div>
     </div>
   );
 };
