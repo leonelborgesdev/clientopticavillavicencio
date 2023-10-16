ddaaa7a970b18d748416c7e661be302de4addfc3
leonelborgesdev <leonel03t@gmail.com>
2023-10-16 00:24:50 -0400

    bootstrap y lista de clientes

--- src/components/Clientes/Clientes.jsx
+++ src/components/Clientes/Clientes.jsx
@@ -1,25 +1,27 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 export const Clientes = () => {
   const customers = useSelector((state) => state.customers);
   return (
     <div>
+      <Link to={"/tienda"}>Volver</Link>
       <h1>Clientes</h1>
-      <table>
+      <table className="table table-striped">
         <thead>
           <tr>
-            <th>Nro</th>
-            <th>Nombre</th>
-            <th>Celular</th>
-            <th>Boton</th>
+            <th scope="col">Nro</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Celular</th>
+            <th scope="col">Boton</th>
           </tr>
         </thead>
         <tbody>
-          {customers.map((cliente) => {
+          {customers.map((cliente, index) => {
             return (
               <tr>
-                <td></td>
+                <td>{index + 1}</td>
                 <td>{cliente.nombre}</td>
                 <td>{cliente.celular}</td>
                 <td></td>
