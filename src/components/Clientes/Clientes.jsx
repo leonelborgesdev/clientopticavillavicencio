b8f5c61a82538a94191e4e451bad239b0b21391f
leonelborgesdev <leonel03t@gmail.com>
2023-10-16 00:11:01 -0400

    lista de clientes

--- src/components/Clientes/Clientes.jsx
+++ src/components/Clientes/Clientes.jsx
@@ -1,5 +1,33 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 export const Clientes = () => {
-  return <div>Clientes</div>;
+  const customers = useSelector((state) => state.customers);
+  return (
+    <div>
+      <h1>Clientes</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Nro</th>
+            <th>Nombre</th>
+            <th>Celular</th>
+            <th>Boton</th>
+          </tr>
+        </thead>
+        <tbody>
+          {customers.map((cliente) => {
+            return (
+              <tr>
+                <td></td>
+                <td>{cliente.nombre}</td>
+                <td>{cliente.celular}</td>
+                <td></td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 };
