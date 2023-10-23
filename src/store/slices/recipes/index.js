14b8949c80a6294b97ba8cbf47d22bf1fb253bff
leonelborgesdev <leonel03t@gmail.com>
2023-10-22 22:00:10 -0400

    getallRecipes

--- src/store/slices/recipes/index.js
+++ src/store/slices/recipes/index.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { api } from "../../api";
 
 const initialState = { list: [] };
 
@@ -15,3 +16,13 @@ export const recipesSlices = createSlice({
 export const { setRecipeList } = recipesSlices.actions;
 
 export default recipesSlices.reducer;
+
+export const getAllRecipes = () => {
+  return async function (dispatch) {
+    const response = await fetch(`${api}/receta`);
+    if (response) {
+      const data = await response.json();
+      console.log(data);
+    }
+  };
+};
